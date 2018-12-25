47f757e4131f6d31b3dfaebf48e89c0fb54fdde6
shuai.pan <shuai.pan@corp.to8to.com>
2018-12-25 09:20:33 +0800

    add sleep time

--- wx/MyWX.py
+++ wx/MyWX.py
@@ -1,3 +1,5 @@
+import time
+
 import itchat
 
 def sendFirstWXmsg():
@@ -12,6 +14,7 @@ def sendFirstWXmsg():
     for friend in friends:
         friend.send("嘿，" +friend.nickName+ "(^_^)：\n 今天每一朵雪花飘下，每一个烟火燃起，每一秒时间流动，都代表着要送你的一个平安祝福。\n--来自潘帅程序员python程序的祝福!突发奇想写了20行不到的代码群发一下祝福！")
         # print("嘿，" +friend.nickName+ "(^_^)：\n 今天每一朵雪花飘下，每一个烟火燃起，每一秒时间流动，都代表着要送你的一个平安祝福。\n--来自潘帅程序员python程序的祝福!突发奇想写了20行不到的代码群发一下祝福！")
+        time.sleep(1)
     # 退出登录
     itchat.logout()
 if __name__ == "__main__":
